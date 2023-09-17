666c9257e0fc7da20c53791e17c55bf64593c144
Humna Ghaffar <88514009+humna567@users.noreply.github.com>
2023-09-17 21:10:34 +0500

    Add files via upload

---
+++ CodeFirst/Model/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CODEFIRST.Model
+{
+      
+     class DatabaseContext : DbContext  // database context child class bangyi dbcontext ki 
+    {
+
+        public DbSet<Employee> Employees { get; set; } // Employees ki property ma data store hoga employee ka , this is generic roperty jo Employee type ki ha , agar mujhy insert update deleete krwaana o ma employee ko usse krungi 
+
+
+    }
+}
---
+++ CodeFirst/Model/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CODEFIRST.Model
+{
+    class Employee // iskay through database mai table banyga
+    {
+        [Key] // anotation ha jo primary key bandeygi 
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public string Designation { get; set; }
+    }
+}
+
+// aik au class banaygy connectivity k liye . is class ko context class kaha jata h 
+//employee ka jitna b data store hoga wo context class ma hoga show 
