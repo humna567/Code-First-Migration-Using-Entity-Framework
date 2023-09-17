1e10bb32ec5b0b03e1306cf6a15d9017fe19e915
Humna Ghaffar <88514009+humna567@users.noreply.github.com>
2023-09-17 21:03:41 +0500

    Add files via upload

---
+++ CodeFirst/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace CODEFIRST
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.NAMEtextBox = new System.Windows.Forms.TextBox();
+            this.GENDERcomboBox = new System.Windows.Forms.ComboBox();
+            this.AGEtextBox = new System.Windows.Forms.TextBox();
+            this.DESIGtextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.codeFirstDBWinformDataSet = new CODEFIRST.CodeFirstDBWinformDataSet();
+            this.employeesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.employeesTableAdapter = new CODEFIRST.CodeFirstDBWinformDataSetTableAdapters.EmployeesTableAdapter();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.codeFirstDBWinformDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeesBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(157, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(367, 57);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "EMPLOYEE DETAILS";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AllowDrop = true;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(177, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "NAME ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(177, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "GENDER";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(177, 164);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "AGE";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(177, 197);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(131, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "DESIGNATION";
+            // 
+            // NAMEtextBox
+            // 
+            this.NAMEtextBox.Location = new System.Drawing.Point(311, 105);
+            this.NAMEtextBox.Name = "NAMEtextBox";
+            this.NAMEtextBox.Size = new System.Drawing.Size(178, 20);
+            this.NAMEtextBox.TabIndex = 1;
+            // 
+            // GENDERcomboBox
+            // 
+            this.GENDERcomboBox.FormattingEnabled = true;
+            this.GENDERcomboBox.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.GENDERcomboBox.Location = new System.Drawing.Point(311, 135);
+            this.GENDERcomboBox.Name = "GENDERcomboBox";
+            this.GENDERcomboBox.Size = new System.Drawing.Size(178, 21);
+            this.GENDERcomboBox.TabIndex = 2;
+            // 
+            // AGEtextBox
+            // 
+            this.AGEtextBox.Location = new System.Drawing.Point(311, 163);
+            this.AGEtextBox.Name = "AGEtextBox";
+            this.AGEtextBox.Size = new System.Drawing.Size(178, 20);
+            this.AGEtextBox.TabIndex = 3;
+            // 
+            // DESIGtextBox
+            // 
+            this.DESIGtextBox.Location = new System.Drawing.Point(311, 197);
+            this.DESIGtextBox.Name = "DESIGtextBox";
+            this.DESIGtextBox.Size = new System.Drawing.Size(178, 20);
+            this.DESIGtextBox.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(167, 242);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 56);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "INSERT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(286, 242);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 56);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "UPDATE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(421, 242);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(103, 56);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "DELETE";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // codeFirstDBWinformDataSet
+            // 
+            this.codeFirstDBWinformDataSet.DataSetName = "CodeFirstDBWinformDataSet";
+            this.codeFirstDBWinformDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // employeesBindingSource
+            // 
+            this.employeesBindingSource.DataMember = "Employees";
+            this.employeesBindingSource.DataSource = this.codeFirstDBWinformDataSet;
+            // 
+            // employeesTableAdapter
+            // 
+            this.employeesTableAdapter.ClearBeforeFill = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(167, 341);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(545, 150);
+            this.dataGridView1.TabIndex = 8;
+            this.dataGridView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.dataGridView1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(775, 514);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.DESIGtextBox);
+            this.Controls.Add(this.AGEtextBox);
+            this.Controls.Add(this.GENDERcomboBox);
+            this.Controls.Add(this.NAMEtextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.codeFirstDBWinformDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeesBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox NAMEtextBox;
+        private System.Windows.Forms.ComboBox GENDERcomboBox;
+        private System.Windows.Forms.TextBox AGEtextBox;
+        private System.Windows.Forms.TextBox DESIGtextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private CodeFirstDBWinformDataSet codeFirstDBWinformDataSet;
+        private System.Windows.Forms.BindingSource employeesBindingSource;
+        private CodeFirstDBWinformDataSetTableAdapters.EmployeesTableAdapter employeesTableAdapter;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ CodeFirst/Form1.cs
@@ -0,0 +1,134 @@
+﻿using CODEFIRST.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CODEFIRST
+{
+    public partial class Form1 : Form
+    {
+        int id = 0;
+        Employee model = new Employee();
+        DatabaseContext db = new DatabaseContext();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void bindGridView()
+        {
+            dataGridView1.DataSource = db.Employees.ToList<Employee>();
+        }
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        void ResetControls()
+        {
+            NAMEtextBox.Clear();
+            GENDERcomboBox.SelectedItem = null;
+            AGEtextBox.Clear();
+            DESIGtextBox.Clear();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            model.Name = NAMEtextBox.Text;
+            model.Gender = GENDERcomboBox.SelectedItem.ToString();
+            model.Age = Convert.ToInt32( AGEtextBox.Text);
+            model.Designation= DESIGtextBox.Text;
+            db.Employees.Add(model);
+          int a =  db.SaveChanges();
+            if(a>0)
+            {
+                MessageBox.Show("Data Inserted!!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                bindGridView();
+                ResetControls();
+            }
+            else
+            {
+                MessageBox.Show("Data Not Inserted!!", "Failed", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            bindGridView();
+
+        }
+
+        private void dataGridView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            id = Convert.ToInt32 ( dataGridView1.SelectedRows[0].Cells[0].Value);
+            model = db.Employees.Where( x => x.Id == id ).FirstOrDefault(); // where keyword is of linq , we use keyword here , // x is refering our model //lambda exp 
+            NAMEtextBox.Text = model.Name;
+            GENDERcomboBox.SelectedItem = model.Gender;
+            AGEtextBox.Text = model.Age.ToString();
+            DESIGtextBox.Text = model.Designation;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            model.Id = id;
+            model.Name = NAMEtextBox.Text;
+            model.Gender = GENDERcomboBox.SelectedItem.ToString();
+            model.Age= Convert .ToInt32( AGEtextBox.Text);
+            model.Designation = DESIGtextBox.Text;
+
+            db.Entry(model).State = EntityState.Modified; // entity state property (modified)
+
+            int a = db.SaveChanges();
+            if (a > 0)
+            {
+                MessageBox.Show("Data Updated!!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                bindGridView();
+                ResetControls();
+            }
+            else
+            {
+                MessageBox.Show("Data Not Updated!!", "Failed", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Are you sure to delete data? ", "Alert" , MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            if ( dr == DialogResult.Yes)
+            {
+                model = db.Employees.Where(x => x.Id == id).FirstOrDefault();
+                db.Entry(model).State = EntityState.Deleted;
+                int a = db.SaveChanges();
+                if (a > 0)
+                {
+                    MessageBox.Show("Data Deleted!!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    bindGridView();
+                    ResetControls();
+                }
+                else
+                {
+                    MessageBox.Show("Data Not Deleted!!", "Failed", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+
+            }
+            else
+            {
+                MessageBox.Show("You have Cancelled the deleted operation.");   
+            }
+        }
+    }
+}
